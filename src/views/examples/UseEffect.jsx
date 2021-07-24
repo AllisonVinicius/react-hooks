9ab5755ef445b70d867a8741728440783845ffd2
AllisonVinicius <allisonvinicius63@gmail.com>
2021-07-23 21:40:37 -0400

    hooks useeffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(n) {
+  if (n < 0) return "Nao existe";
+  if (n === 0) return 0;
+  return calcFatorial((n - 1) * n);
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    //efeito colateral
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number] //array de dependecia, tdas vez modificar  o estado e disparada o efeito
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">{fatorial} </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
