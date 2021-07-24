38143469cd71543a80314f6b96f8335bc4c37a59
AllisonVinicius <allisonvinicius63@gmail.com>
2021-07-23 23:00:16 -0400

    useeffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-function calcFatorial(n) {
-  if (n < 0) return "Nao existe";
-  if (n === 0) return 0;
-  return calcFatorial((n - 1) * n);
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
 const UseEffect = (props) => {
   const [number, setNumber] = useState(1);
   const [fatorial, setFatorial] = useState(1);
+  const [oque, setOque] = useState("impar");
 
   useEffect(
     //efeito colateral
     function () {
       setFatorial(calcFatorial(number));
     },
-    [number] //array de dependecia, tdas vez modificar  o estado e disparada o efeito
+    [number]
+  ); //array de dependecia, tdas vez modificar  o estado e disparada o efeito
+
+  useEffect(
+    function () {
+      if (fatorial > 1000000) {
+        document.title = "Eita";
+      }
+    },
+    [fatorial]
+  );
+
+  useEffect(
+    function () {
+      setOque(number % 2 === 0 ? "par" : "imapr");
+    },
+    [number]
   );
 
   return (
@@ -25,10 +44,13 @@ const UseEffect = (props) => {
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
+      <SectionTitle title="exercicio #01" />
       <div className="center">
         <div>
           <span className="text">Fatorial: </span>
-          <span className="text red">{fatorial} </span>
+          <span className="text red">
+            {fatorial === -1 ? "nao existe" : fatorial}{" "}
+          </span>
         </div>
         <input
           type="number"
@@ -36,6 +58,14 @@ const UseEffect = (props) => {
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
+      </div>
+
+      <SectionTitle title="Exercicio #02" />
+      <div className="center">
+        <div>
+          <span className="text">É par ou nao: </span>
+          <span className="text red">{oque}</span>
+        </div>
       </div>
     </div>
   );
